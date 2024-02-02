5e250f11dc6b2b0d2eee99096637b24d45c56626
Fidel Imasuen <fetinosa@gmail.com>
2024-02-02 04:39:34 -0800

    changed the top_up function a bit

--- TopUp_Airtime/top_up.py
+++ TopUp_Airtime/top_up.py
@@ -1,8 +1,13 @@
-def top_up_airtime(service_provider, amount):
+def top_up_airtime(service_provider,phone_number,amount):
     service_provider_list= ['MTN','AIRTEL','GLO','ETISALAT']
     service_provider= service_provider.upper()
     service_provider_flag = 0
     amount = amount
+
+    if len(phone_number) != 11:
+        return "invalid phone number"
+    else:
+        pass
 
     if service_provider.upper() in service_provider_list:
         service_provider_flag = 1
