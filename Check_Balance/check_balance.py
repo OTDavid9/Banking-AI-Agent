52d6ea5bbfa48bbe1e11f9631e61fe8d15d67086
OTDavid <tunjiobidokun@gmail.com>
2024-02-02 04:33:42 +0100

    Modified complaints.py and Check_balance.py

--- Check_Balance/check_balance.py
+++ Check_Balance/check_balance.py
@@ -24,3 +24,42 @@ def Check_balance(email, password):
 Check_balance('jane.smith@email.com', 'janeSmith@123')
 Check_balance("john.doe@email.com",'johnDoe@123')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import sqlite3
+
+# def Check_balance(email, password):
+#     # Connect to the SQLite database
+#     conn = sqlite3.connect('bank_database.db')
+
+#     # Create a cursor object to execute SQL queries
+#     cursor = conn.cursor()
+
+#     # Fetch the account balance based on the email and password
+#     cursor.execute("SELECT account_balance FROM customers WHERE email = ? AND password = ?", (email, password))
+#     balance = cursor.fetchone()
+
+#     # Check if the email and password combination exists in the database
+#     if balance:
+#         return print(f"Account Balance for {email}: ${balance[0]}")
+#     else:
+#         return (f"Invalid email or password for {email}.")
+
+#     # Close the database connection
+#     conn.close()
+
+# # Example usage
+# Check_balance('jane.smith@email.com', 'janeSmith@123')
+# Check_balance("john.doe@email.com",'johnDoe@123')
+
--- Complaints/complaints.py
+++ Complaints/complaints.py
@@ -1,9 +1,19 @@
-import json 
-question = input("Enter your query here : ")
 
+
+
+# def complaints(complaints_query=question):
+    
+#     complaints_url = "https://drive.google.com/file/d/1AlUXgJoRmNqQJr9tLE0mr5DazH5Bp7ez/view?usp=drive_link"
+
+#     # Return the response from Azure Cognitive Search
+#     return  json.dumps(complaints_url)
+
+import json 
+
+question = input("Enter your query here : ")
 def complaints(complaints_query=question):
     
-    complaints_url = "www.wemabank.com/complaints"
+    complaints_url = "https://drive.google.com/file/d/1AlUXgJoRmNqQJr9tLE0mr5DazH5Bp7ez/view?usp=drive_link"
 
     # Return the response from Azure Cognitive Search
-    return  json.dumps(complaints_url)
+    return  json.dumps({"Lodge your complaint using this link":complaints_url, "complaints_query": complaints_query})
